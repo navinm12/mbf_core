3357548a25ac1deed45f1af9ba414acab6545f65
navinm12 <navinmurugaiyan12@gmail.com>
2024-07-29 10:59:26 +0530

    added userdialog

--- src/dialogs/userdialog.ts
+++ src/dialogs/userdialog.ts
@@ -37,25 +37,46 @@ export class UserdetailDialog extends ComponentDialog {
     
     private async Step1(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
     
-        
+        const messageText = 'May i know your name?';
+            const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
     }
 
    
     private async Step2(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-      
+        const userDetails = stepContext.options as UserDetails;
+        userDetails.name=stepContext.result
+
+        const messageText = 'where are you from?';
+        const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+        return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
     }
 
     
     private async Step3(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-   
+        const userDetails = stepContext.options as UserDetails;
+        userDetails.region=stepContext.result
+
+        const messageText = 'what is your age?';
+            const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
     }
 
     private async Step4(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-     
+        const userDetails = stepContext.options as UserDetails;
+        userDetails.age=stepContext.result
+        const messageText = 'what is your hobby!';
+        const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+        return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
     }
 
    
     private async Step5(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-        
+        const userDetails = stepContext.options as UserDetails;
+        userDetails.hobby=stepContext.result
+        const messageText = `Greetings ${userDetails.name}, we acknowledge that you are ${userDetails.age} years old and are based in ${userDetails.region}. We also noted that your hobby is ${userDetails.hobby}. We appreciate your presence and look forward to further interactions. Have a productive day ahead!`;
+        const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+        await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
+        return await stepContext.endDialog();
     }
 }
